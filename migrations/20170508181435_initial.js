721bfe745660241aceb22d286ef76f95b59a7db2
adriancarriger <acarriger@docutap.com>
2017-05-09 20:36:52 -0500

    refactor(migration): move functions inside Promise.all

--- migrations/20170508181435_initial.js
+++ migrations/20170508181435_initial.js
@@ -3,36 +3,37 @@ const knexConfig = require('../knexfile.js');
 const knex = knexFunction(knexConfig.development);
 
 exports.up = function(knex, Promise) {
-  // Messages
-  const addMessages = knex.schema.createTable('messages', message => {
-    message.text('body').notNull();
-    message.string('sid').primary();
-    message.dateTime('dateCreated').notNull();
-    message.enum('direction', ['inbound', 'outbound']).notNull();
-    message.string('from', 12).index().references('number').inTable('numbers').notNull();
-    message.string('to', 12).index().references('number').inTable('numbers').notNull();
-  });
-  // Numbers
-  const addNumbers = knex.schema.createTable('numbers', number => {
-    number.string('number', 12).primary();
-    number.dateTime('dateCreated').notNull();
-  });
-  // People
-  const addPeople = knex.schema.createTable('people', person => {
-    person.increments('id').primary();
-    person.string('firstName');
-    person.string('lastName');
-    person.string('job');
-    person.dateTime('dateCreated').notNull();
-  });
-  // Many to many => numbers to people
-  const addNumbersToPeople = knex.schema.createTable('numbersToPeople', table => {
-    table.increments('id').primary();
-    table.string('number', 12).index().references('number').inTable('numbers').notNull();
-    table.bigInteger('personId').index().references('id').inTable('people').notNull();
-    table.dateTime('dateCreated').notNull();
-  });
-  return Promise.all([addMessages, addNumbers, addPeople, addNumbersToPeople]);
+  return Promise.all([
+    // Messages
+    knex.schema.createTable('messages', message => {
+      message.text('body').notNull();
+      message.string('sid').primary();
+      message.dateTime('dateCreated').notNull();
+      message.enum('direction', ['inbound', 'outbound']).notNull();
+      message.string('from', 12).index().references('number').inTable('numbers').notNull();
+      message.string('to', 12).index().references('number').inTable('numbers').notNull();
+    }),
+    // Numbers
+    knex.schema.createTable('numbers', number => {
+      number.string('number', 12).primary();
+      number.dateTime('dateCreated').notNull();
+    }),
+    // People
+    knex.schema.createTable('people', person => {
+      person.increments('id').primary();
+      person.string('firstName');
+      person.string('lastName');
+      person.string('job');
+      person.dateTime('dateCreated').notNull();
+    }),
+    // Many to many => numbers to people
+    knex.schema.createTable('numbersToPeople', table => {
+      table.increments('id').primary();
+      table.string('number', 12).index().references('number').inTable('numbers').notNull();
+      table.bigInteger('personId').index().references('id').inTable('people').notNull();
+      table.dateTime('dateCreated').notNull();
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
