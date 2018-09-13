18b5e643f6a61d3219d9b96a62cb23b2df20b9d1
Adrian Carriger <adriancarriger@users.noreply.github.com>
2018-09-12 21:29:01 -0500

    refactor(app): remove unused code

--- src/twilio/get-messages.js
+++ src/twilio/get-messages.js
@@ -1,8 +1,6 @@
 const config = require('../../config/twilio-config');
 const client = require('twilio')(config.accountSid, config.authToken);
 
-const screen = require('../util/screen');
-
 function getMessages() {
   return new Promise((resolve, reject) => {
     client.messages.list((error, data) => {
